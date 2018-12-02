ee4f143de32d3e57c46e4e7ab5619fa0fe39cceb
Hoang Trung <ch4initus@gmail.com>
2018-12-03 00:38:06 +0700

    Create project and add Header, Footer, Login Page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Login from './components/login';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Login/>
+      </div>
+    );
+  }
+}
+
+export default App;
