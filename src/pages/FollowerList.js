3e373f5806dbf423b7979dfc43df16a5dc908245
Hoang Trung <ch4initus@gmail.com>
2018-12-26 10:43:32 +0700

    add pagination

--- src/pages/FollowerList.js
+++ src/pages/FollowerList.js
@@ -21,34 +21,36 @@ class FollowerList extends Component {
                     <div id="page-contents">
                         <div className="row">
                             <div className="col-md-3"></div>
-                            <div className="col-md-6">
+                            <div className="col-md-7">
                                 <div className="friend-list">
                                     <div className="row">
                                         {this.showFollowerList(follows)}
                                     </div>
                                 </div>
                             </div>
-                            <div className='row'>
-                                <div className="col-md-3">
-                                    <nav aria-label="..." style={{ marginRight: '0px', right: '0px' }}>
-                                        <ul className="pagination">
-                                            <li className="page-item disabled">
-                                                <span className="page-link">Previous</span>
-                                            </li>
-                                            <li className="page-item"><a className="page-link" href="#">1</a></li>
-                                            <li className="page-item active">
-                                                <span className="page-link">
-                                                    2
-                                            <span className="sr-only">(current)</span>
-                                                </span>
-                                            </li>
-                                            <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                            <li className="page-item">
-                                                <a className="page-link" href="#">Next</a>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                </div>
+                            
+                        </div>
+                        <div className='row'>
+                            <div className="col-md-9"></div>
+                            <div className="col-md-3" style={{display: 'flex', alignItems: 'right'}}>
+                                <nav aria-label="..." style={{ marginRight: '0px', right: '0px' }}>
+                                    <ul className="pagination">
+                                        <li className="page-item disabled">
+                                            <span className="page-link">Previous</span>
+                                        </li>
+                                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                        <li className="page-item active">
+                                            <span className="page-link">
+                                                2
+                                        <span className="sr-only">(current)</span>
+                                            </span>
+                                        </li>
+                                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                        <li className="page-item">
+                                            <a className="page-link" href="#">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>
                             </div>
                         </div>
                     </div>
--- src/pages/NewFeed.js
+++ src/pages/NewFeed.js
@@ -42,6 +42,29 @@ class NewFeed extends Component {
                                 <FollowHint />
                             </div>
                         </div>
+                        <div className='row'>
+                            <div className="col-md-9"></div>
+                            <div className="col-md-3" style={{display: 'flex', alignItems: 'right'}}>
+                                <nav aria-label="..." style={{ marginRight: '0px', right: '0px' }}>
+                                    <ul className="pagination">
+                                        <li className="page-item disabled">
+                                            <span className="page-link">Previous</span>
+                                        </li>
+                                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                        <li className="page-item active">
+                                            <span className="page-link">
+                                                2
+                                        <span className="sr-only">(current)</span>
+                                            </span>
+                                        </li>
+                                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                        <li className="page-item">
+                                            <a className="page-link" href="#">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <Footer />
